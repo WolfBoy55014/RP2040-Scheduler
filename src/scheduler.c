590ac5947bc03102a9c639ec165edb51e5c38755
WolfBoy55014 <97041213+WolfBoy55014@users.noreply.github.com>
2025-11-23 07:45:58 -0600

    fixed some messy git stuff

--- src/scheduler.c
+++ src/scheduler.c
@@ -448,14 +448,6 @@ void start_systick() {
                       M0PLUS_SYST_CSR_ENABLE_BITS;      // enable SysTick
 }
 
-int32_t refresh_systick() {
-    return start_systick();
-}
-
-int32_t refresh_systick() {
-    return start_systick();
-}
-
 void remove_task(task_t *task) {
     const uint32_t saved_irq = scheduler_spin_lock();
 
