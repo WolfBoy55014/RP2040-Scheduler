2a1d6c199d9cc459eb461781aa8ab2e835494d4b
WolfBoy55014 <97041213+WolfBoy55014@users.noreply.github.com>
2026-02-27 16:56:02 -0600

    Created Files for malloc wrapper

---
+++ include/lib/memory_wrapper.h
@@ -0,0 +1,10 @@
+//
+// Created by wolfboy on 2/26/2026.
+//
+
+#ifndef KELPOS_LITE_MEMORY_WRAPPER_H
+#define KELPOS_LITE_MEMORY_WRAPPER_H
+
+#include <cstdint>
+
+#endif //KELPOS_LITE_MEMORY_WRAPPER_H
---
+++ src/memory_wrapper.c
@@ -0,0 +1,6 @@
+//
+// Created by wolfboy on 2/26/2026.
+//
+
+#include "memory_wrapper.h"
+
